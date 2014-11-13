ba3dc297388308ad17d7924f8c0b6d6caf24d92b
Mironor <meanor89@gmail.com>
2014-11-13 21:20:50 +0100

    There is no need to inject directly in the test, but OfficialMessageService still needs implicit injector

--- test/OfficialMessageServiceSpec.scala
+++ test/OfficialMessageServiceSpec.scala
@@ -3,25 +3,24 @@ import play.api.GlobalSettings
 import play.api.test.{FakeApplication, PlaySpecification}
 import scaldi.Injectable
 import scaldi.play.ScaldiSupport
-import service.MessageService
+import service.OfficialMessageService
 
-class MessageServiceSpec extends PlaySpecification with Injectable {
+class OfficialMessageServiceSpec extends PlaySpecification with Injectable {
 
   object TestGlobal extends GlobalSettings with ScaldiSupport {
     def applicationModule = new WebModule :: new UserModule
   }
 
-  // As this class is declared as Injectable, we should provide implicit injector
   implicit lazy val injector = TestGlobal.injector
+
 
   "Message Service" should {
     "provide greeting message" in {
       running(FakeApplication(withGlobal = Some(TestGlobal))) {
 
-        // Injecting the service
-        val messageService = inject[MessageService]
-
-        val expectedMessage = "Hi, test"
+        // Implicit injector is needed here
+        val messageService = new OfficialMessageService
+        val expectedMessage = "Welcome, test!"
         val message = messageService.getGreetMessage("test")
 
         message must beEqualTo(expectedMessage)
